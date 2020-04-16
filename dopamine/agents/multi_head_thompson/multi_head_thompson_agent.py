ef869801b34a3cfd310de02fb093a6ef2420b7e8
alhamzah <alhamzah@matlaber4.media.mit.edu>
2020-04-15 22:07:25 -0400

    improved logging and clean up

--- dopamine/agents/multi_head_thompson/multi_head_thompson_agent.py
+++ dopamine/agents/multi_head_thompson/multi_head_thompson_agent.py
@@ -31,7 +31,7 @@ class MultiHeadThompsonAgent(multi_head_ddqn_agent.MultiHeadDDQNAgent):
         sess, num_actions, **kwargs)
 
     self.arms = [{'success': 0, 'failure': 0} for _ in range(self.num_heads)]
-    self._update_q_argmax()
+    self.cur_head = 0
 
   def _thompson_sample(self):
     probs = []
@@ -40,17 +40,58 @@ class MultiHeadThompsonAgent(multi_head_ddqn_agent.MultiHeadDDQNAgent):
         probs.append(p)
     return probs
 
-  def _update_q_argmax(self):
+  def _compute_q_argmax(self):
     self.cur_head = np.argmax(self._thompson_sample())
-    x = tf.gather(self._net_outputs.q_heads, self.cur_head, axis=2)
-    self._q_argmax = tf.argmax(x, axis=1)[0]
+    x = self._sess.run(self._net_outputs.q_heads,
+                       {self.state_ph: self.state})
+    return np.argmax(x[:,:,self.cur_head], axis=1)[0]
     
   def _update_thompson(self, selected_head, reward):
     reward = int(reward > self.thompson_cutoff)
     self.arms[selected_head]['success'] += reward
     self.arms[selected_head]['failure'] += 1-reward
-    self._update_q_argmax()
     
   def _store_transition(self, last_observation, action, reward, is_terminal):
     self._update_thompson(self.cur_head, reward)
     self._replay.add(last_observation, action, reward, is_terminal)
+
+  def _select_action(self):
+    """Select an action from the set of available actions.
+
+    Chooses an action randomly with probability self._calculate_epsilon(), and
+    otherwise acts greedily according to the current Q-value estimates.
+
+    Returns:
+       int, the selected action.
+    """
+    if self.eval_mode:
+      self._log_values()
+      epsilon = self.epsilon_eval
+    else:
+      epsilon = self.epsilon_fn(
+          self.epsilon_decay_period,
+          self.training_steps,
+          self.min_replay_history,
+          self.epsilon_train)
+    if random.random() <= epsilon:
+      # Choose a random action with probability epsilon.
+      return random.randint(0, self.num_actions - 1)
+    else:
+      # Choose the action with highest Q-value at the current state according
+      # to the current head.
+      return self._compute_q_argmax()
+
+  def _log_values(self):
+      if self.log_counter%self.log_frequency == 0:
+        #save the q values
+        with open(self.log_dir+'/q_heads_values.txt', 'ab') as f:
+          x = self._sess.run(self._net_outputs.q_heads,
+                             {self.state_ph: self.state})
+          for i in range(len(x)):
+            np.savetxt(f, x[i])
+          np.savetxt(f, [0])
+        with open(self.log_dir+'/thompson_probs.txt', 'ab') as f:
+          x = self._thompson_sample()
+          np.savetxt(f, np.array([x]))
+
+      self.log_counter += 1
--- dopamine/agents/multi_head_ucb/multi_head_ucb_agent.py
+++ dopamine/agents/multi_head_ucb/multi_head_ucb_agent.py
@@ -4,7 +4,6 @@ import collections
 import os
 import math
 
-from dopamine.agents.multi_head import atari_helpers
 from dopamine.agents.multi_head import multi_head_ddqn_agent
 import gin
 import tensorflow as tf
@@ -42,7 +41,6 @@ class MultiHeadUCBAgent(multi_head_ddqn_agent.MultiHeadDDQNAgent):
     self.arms = [{'sum': 0, 'count': 0} for _ in range(self.num_heads)]
     self.rewards = [collections.deque([0]*self.ucb_horizon)
                     for _ in range(self.num_heads)]
-    self._update_q_argmax()
 
   def _compute_ucb(self):
     bounds = []
@@ -56,10 +54,11 @@ class MultiHeadUCBAgent(multi_head_ddqn_agent.MultiHeadDDQNAgent):
         bounds.append(avg + ucb)
     return bounds
 
-  def _update_q_argmax(self):
+  def _compute_q_argmax(self):
     self.cur_head = np.argmax(self._compute_ucb())
-    x = tf.gather(self._net_outputs.q_heads, self.cur_head, axis=2)
-    self._q_argmax = tf.argmax(x, axis=1)[0]
+    x = self._sess.run(self._net_outputs.q_heads,
+                       {self.state_ph: self.state})
+    return np.argmax(x[:,:,self.cur_head], axis=1)[0]
     
   def _update_ucb(self, selected_head, reward):
     self.ucb_count += 1
@@ -68,8 +67,48 @@ class MultiHeadUCBAgent(multi_head_ddqn_agent.MultiHeadDDQNAgent):
     self.arms[selected_head]['sum'] += reward
     self.rewards[selected_head].pop()
     self.rewards[selected_head].appendleft(reward)
-    self._update_q_argmax()
     
   def _store_transition(self, last_observation, action, reward, is_terminal):
     self._update_ucb(self.cur_head, reward)
     self._replay.add(last_observation, action, reward, is_terminal)
+
+  def _select_action(self):
+    """Select an action from the set of available actions.
+
+    Chooses an action randomly with probability self._calculate_epsilon(), and
+    otherwise acts greedily according to the current Q-value estimates.
+
+    Returns:
+       int, the selected action.
+    """
+    if self.eval_mode:
+      self._log_values()
+      epsilon = self.epsilon_eval
+    else:
+      epsilon = self.epsilon_fn(
+          self.epsilon_decay_period,
+          self.training_steps,
+          self.min_replay_history,
+          self.epsilon_train)
+    if random.random() <= epsilon:
+      # Choose a random action with probability epsilon.
+      return random.randint(0, self.num_actions - 1)
+    else:
+      # Choose the action with highest Q-value at the current state according
+      # to the current head.
+      return self._compute_q_argmax()
+
+  def _log_values(self):
+      if self.log_counter%self.log_frequency == 0:
+        #save the q values
+        with open(self.log_dir+'/q_heads_values.txt', 'ab') as f:
+          x = self._sess.run(self._net_outputs.q_heads,
+                             {self.state_ph: self.state})
+          for i in range(len(x)):
+            np.savetxt(f, x[i])
+          np.savetxt(f, [0])
+        with open(self.log_dir+'/ucb_values.txt', 'ab') as f:
+          x = self._compute_ucb()
+          np.savetxt(f, np.array([x]))
+
+      self.log_counter += 1
